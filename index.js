dc73720e2b967fe6ad23ddecd6d8dcb3c13e01e0
Elena <kunstvanelena@gmail.com>
2023-05-10 17:05:35 +0900

    added functions to create and activate dots

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const slides = document.querySelectorAll('.slide');
 const btnR = document.querySelector('.slider--btn__right')
 const btnL = document.querySelector('.slider--btn__left')
+const dotContainer = document.querySelector('.dotContainer')
 const maxSlide = slides.length;
 let curSlide = 0;
 
-toSlide(0)
+const init = function () {
+    toSlide(0);
+    createDots();
+    activateDot(0)
+}();
 
+
+//---- Event Handler ----//
 btnR.addEventListener('click', nextSlide)
 btnL.addEventListener('click', previousSlide)
 document.addEventListener('keydown', (e) => {
     e.key === 'ArrowRight' && nextSlide(); //short-circuiting
     e.key === 'ArrowLeft' && previousSlide();
-
+})
+dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots_dot')) {
+        const slide = e.target.dataset.slide;
+        toSlide(slide);
+    }
 })
 
+
+//---- Functions ----//
 function nextSlide() {
     if (curSlide === maxSlide -1) {
         curSlide = 0;
@@ -21,6 +35,7 @@ function nextSlide() {
         curSlide++;
     }
     toSlide(curSlide)
+    activateDot(curSlide)
 }
 
 function previousSlide() {
@@ -30,11 +45,26 @@ function previousSlide() {
         curSlide--;
     }
     toSlide(curSlide)
-
+    activateDot(curSlide)
 }
 
 function toSlide(curSlide) {    
     slides.forEach((s, i) => {
         s.style.transform = `translateX(${100 * (i - curSlide)}%)`
     })   
+}
+
+function createDots() {
+    slides.forEach(function (_, i) {
+        dotContainer.insertAdjacentHTML('beforeend', `
+        <button class="dots_dot" data-slide="${i}"></button`)   
+    })
+}
+
+
+function activateDot(slide) {
+    document.querySelectorAll('.dots_dot')
+        .forEach(dot => dot.classList.remove('active'));
+    document.querySelector(`.dots_dot[data-slide='${slide}']`)
+        .classList.add('active')
 }
